30c65a6ad707d7fb0b328a239e45c673d0f37fdf
josh-2234 <joshua.braganza22@ycdsbk12.ca>
2019-09-18 09:39:01 -0400

    The black market

---
+++ Working/Pactice questions/1_1_1 printing.py
@@ -0,0 +1,22 @@
+print("sup")
+print("ianchode")
+print ("hi there")
+print ("my favourtite colour is magenta") 
+print ("(\___/)") 
+print (" \* */")
+print   ("  \ /")
+print    ("   o")
+print ("josh \n" , 15)
+a = 10 
+c = 20 
+b = c 
+d = a + b
+f = 50
+e = f + d
+print (a) 
+print (b)
+print (c)
+print (d)
+print (e)
+j = b + c
+print (j)
