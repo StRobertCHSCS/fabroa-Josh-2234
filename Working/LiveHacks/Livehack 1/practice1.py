590cb5313a0b147faec095208c9f56605ddeb7c1
josh-2234 <joshua.braganza22@ycdsbk12.ca>
2019-10-01 09:46:13 -0400

    practise

---
+++ Working/LiveHacks/Livehack 1/practice1.py
@@ -0,0 +1,19 @@
+'''
+-------------------------------------------------------------------------------
+Name:		filename.py
+Purpose:	comnvert fahrenheit to celsius
+
+Author:	Branaza J
+
+Created:	date in 01/10/2019
+------------------------------------------------------------------------------
+'''
+
+# get fahrneheit from user 
+fahrenheit = float(input("enter the fahrenheit: "))
+
+# compute celsius from fahrenheit
+celsius = 5/9 * (fahrenheit - 32)
+
+# output celsius 
+print("the temprature in celsius is", celsius)
